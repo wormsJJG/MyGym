281dcb1f2a35c6a6a200775002c016104e94cf1e
wormsJJG <jjr66461837@gmail.com>
2022-06-07 21:43:40 +0900

    xcode 프로젝트 생성

---
+++ MyGym/MyGym/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyGym
+//
+//  Created by 정재근 on 2022/06/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
