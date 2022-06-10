fb0e581e95c1b694ce676bb21c82a739d4827e85
bloodparty297@gmail.com <schro@Schroui-MacBookPro.local>
2022-06-10 22:17:32 +0900

    헬스장 목록 뷰의 컬렉션뷰 셀 가로, 세로 길이 고정

---
+++ MyGym/MyGym/Controller/GymListCellViewController.swift
@@ -0,0 +1,23 @@
+//
+//  GymListCellViewController.swift
+//  MyGym
+//
+//  Created by Schro on 2022/06/10.
+//
+
+import UIKit
+
+class GymListCellViewController: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- MyGym/MyGym/Controller/GymListViewController.swift
+++ MyGym/MyGym/Controller/GymListViewController.swift
@@ -8,22 +8,22 @@
 import UIKit
 
 class GymListViewController: UIViewController {
-
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    }
+}
+extension GymListViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        return cell
     }
-    */
-
+    
+    
+    
 }
