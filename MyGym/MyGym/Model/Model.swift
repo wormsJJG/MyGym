cc14c88fb7f047d79837190536ca51c84c03ce77
wormsJJG <jjr66461837@gmail.com>
2022-06-11 20:18:42 +0900

    Model 생성

---
+++ MyGym/MyGym/Model/Model.swift
@@ -0,0 +1,37 @@
+//
+//  Model.swift
+//  MyGym
+//
+//  Created by 정재근 on 2022/06/11.
+//
+
+import Foundation
+
+class Auth {
+    var id: String = ""
+    var password: String = ""
+    var uid: String = "" //foreign key
+}
+class Users {
+    var uid: String = "" // primary key
+    var name: String = ""
+    var attendingHealthClub: HealthClub = HealthClub()
+    var ptDate: [PtDate] = []
+}
+class HealthClub{
+    var uid: String = "" //primary key
+    var clubName: String = ""
+    var clubLocation: String = ""
+    var clubPhoneNumber: String = ""
+    //clubImages
+    var trainerList: [Trainer] = []
+}
+class Trainer {
+    var uid: String = "" // primary key
+    var name: String = ""
+    var ptDate: [PtDate] = []
+    var workPlace: HealthClub = HealthClub()
+}
+class PtDate{
+    var date: Date = Date()
+}
