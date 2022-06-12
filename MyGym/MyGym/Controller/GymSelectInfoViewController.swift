15e7018dfc975b436feffd3179cbd810c3aae731
bloodparty297@gmail.com <schro@Schroui-MacBookPro.local>
2022-06-12 15:46:53 +0900

    헬스장 정보 뷰의 추가버튼 알림 생성

--- MyGym/MyGym/Controller/GymSelectInfoViewController.swift
+++ MyGym/MyGym/Controller/GymSelectInfoViewController.swift
@@ -12,18 +12,13 @@ class GymSelectInfoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func addButtonClick(_ sender: Any) {
     }
-    */
-
+    func addAlert(_ title: String, _ message: String){
+        let addAlert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        let okAction = UIAlertAction(title: "확인", style: .default){ (action) in }
+        addAlert.addAction(okAction)
+        present(addAlert, animated: true)
+    }
 }
