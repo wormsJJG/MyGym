986c85a9eb9f2da4b569fe0d2c1ca95649bf6a21
bloodparty297@gmail.com <schro@Schroui-MacBookPro.local>
2022-06-12 14:43:26 +0900

    헬스장 목록 추가를 위한 헬스장 선택 뷰의 틀 제작

---
+++ MyGym/MyGym/Controller/GymSelectViewController.swift
@@ -0,0 +1,16 @@
+//
+//  GymSelectViewController.swift
+//  MyGym
+//
+//  Created by Schro on 2022/06/12.
+//
+
+import UIKit
+
+class GymSelectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
