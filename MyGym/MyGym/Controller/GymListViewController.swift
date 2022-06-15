d398f8de10763fc74e9996921ee54c75ba4326b5
bloodparty297@gmail.com <schro@Schroui-MacBookPro.local>
2022-06-16 01:14:46 +0900

    헬스장 목록 뷰를 유저 페이지 뷰로 변경하고 그에 맞춰 GymListViewController의 컬렉션뷰 관련 코드를 제거.

--- MyGym/MyGym/Controller/GymListViewController.swift
+++ MyGym/MyGym/Controller/GymListViewController.swift
@@ -12,7 +12,6 @@ class GymListViewController: UIViewController {
     let type = "HealthClub"
     let db = Database.database().reference().child("users")
     var gymList: [Gym] = []
-    @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,27 +32,10 @@ class GymListViewController: UIViewController {
                     gym.location = item?["location"] as! String
                     gym.type = item?["type"] as! String
                     self.gymList.append(gym)
-                    self.collectionView.reloadData()
                 }
             }
         })
     }
-    
-}
-extension GymListViewController: UICollectionViewDelegate, UICollectionViewDataSource{
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return gymList.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! GymListCollectionViewCell
-        cell.layer.cornerRadius = 12.0
-        cell.gymNameLabel.text = gymList[indexPath.item].name
-        cell.detailLabel.text = gymList[indexPath.item].location
-        return cell
-    }
-    
-    
     
 }
 struct Gym: Codable{
