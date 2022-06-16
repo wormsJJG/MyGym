889630b7d3e7b663e78a37795bc08a877d98a62c
wormsJJG <jjr66461837@gmail.com>
2022-06-16 21:35:22 +0900

    cell 파일 복구

---
+++ MyGym/MyGym/Controller/cell/GymListCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  GymListCollectionViewCell.swift
+//  MyGym
+//
+//  Created by 정재근 on 2022/06/12.
+//
+
+import UIKit
+
+class GymListCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var gymNameLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    
+}
