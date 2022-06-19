c28d17e16128a049325607dbfce8c126286a9085
bloodparty297@gmail.com <schro@Schroui-MacBookPro.local>
2022-06-19 22:57:27 +0900

    중간시점 저장

---
+++ MyGym/MyGym/Controller/AccountInfoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AccountInfoViewController.swift
+//  MyGym
+//
+//  Created by Schro on 2022/06/19.
+//
+
+import UIKit
+import FirebaseAuth
+import FirebaseDatabase
+class AccountInfoViewController: UIViewController {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
