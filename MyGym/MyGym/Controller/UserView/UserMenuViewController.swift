65a73b6665a254d3d3145615e18ab6708f8691fb
bloodparty297@gmail.com <schro@Schroui-MacBookPro.local>
2022-06-28 12:12:11 +0900

    유저메뉴 uiview 삭제

--- MyGym/MyGym/Controller/UserView/UserMenuViewController.swift
+++ MyGym/MyGym/Controller/UserView/UserMenuViewController.swift
@@ -8,34 +8,8 @@
 import UIKit
 
 class UserMenuViewController: UIViewController {
-    @IBOutlet weak var myGymView: UIView!
-    @IBOutlet weak var myPageView: UILabel!
     var userUid: String = ""
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        viewSetUp()
-    }
-    func viewSetUp(){
-        setGesture()
-    }
-    func setGesture(){
-        let myGymGesture = UITapGestureRecognizer(target: self, action:  #selector(self.myGymClick))
-        self.myGymView.addGestureRecognizer(myGymGesture)
-        let myPageGesture = UITapGestureRecognizer(target: self, action: #selector(self.myPageClick))
-        self.myPageView.addGestureRecognizer(myPageGesture)
-    }
-    @objc func myGymClick(sender : UITapGestureRecognizer) {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let testVC = storyboard.instantiateViewController(withIdentifier: "TestVC") as! MyPageViewController
-        testVC.userUid = self.userUid
-        self.navigationController?.pushViewController(testVC, animated: true)
-        
-    }
-    @objc func myPageClick(sender : UITapGestureRecognizer){
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let userInfoVC = storyboard.instantiateViewController(withIdentifier: "UserInfoVC") as! AccountInfoViewController
-        userInfoVC.userUid = self.userUid
-        self.navigationController?.pushViewController(userInfoVC, animated: true)
     }
 }
